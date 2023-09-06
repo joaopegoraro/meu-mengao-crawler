436e019c07b81eeff2404ce5b5652999e4ac865b
João Pegoraro <hahmraro@tuta.io>
2023-09-05 22:22:01 -0300

    fixed script not being able to be executed outside of src folder

--- scraper.js
+++ scraper.js
@@ -3,17 +3,31 @@ const noticias = require("./dist/noticias-scraper.js");
 const youtube = require("./dist/youtube-scraper.js");
 const campeonatos = require("./dist/campeonatos-scraper.js");
 
-db.startConnection().then(async () => {
-  const startTime = new Date().toUTCString();
-  console.log(`STARTED SCRAPING: ${startTime}`);
+try {
+  db.startConnection().then(async () => {
+    try {
+      const startTime = new Date().toUTCString();
+      console.log(`STARTED SCRAPING: ${startTime}`);
 
-  await noticias.scrapeNoticias();
-  await youtube.scrapeYoutube();
-  await campeonatos.scrapeCampeonatos();
+      await noticias.scrapeNoticias();
+      await youtube.scrapeYoutube();
+      await campeonatos.scrapeCampeonatos();
 
-  db.endConnection();
+      db.endConnection();
 
+      console.log(
+        `FINISHED SCRAPING:\nStart time: ${startTime}\nEnd time: ${new Date().toUTCString()}`
+      );
+    } catch (e) {
+      console.log(e);
+      console.log(
+        `FINISHED SCRAPING (WITH ERROR):\nStart time: ${startTime}\nEnd time: ${new Date().toUTCString()}`
+      );
+    }
+  });
+} catch (e) {
+  console.log(e);
   console.log(
-    `FINISHED SCRAPING:\nStart time: ${startTime}\nEnd time: ${new Date().toUTCString()}`
+    `FINISHED SCRAPING (WITH ERROR):\nStart time: ${startTime}\nEnd time: ${new Date().toUTCString()}`
   );
-});
+}
